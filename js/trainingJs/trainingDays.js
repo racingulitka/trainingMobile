9413b863db53522d502144dde6c5e200af346484
Maksim <mex83011@gmail.com>
2023-03-14 17:52:00 +0300

    code review

--- js/trainingJs/trainingDays.js
+++ js/trainingJs/trainingDays.js
@@ -1,4 +1,4 @@
-﻿
+﻿let excerciseCondition = ["", true, true, true, true, true, true, true];
 
 window.onload = () =>{
 
@@ -15,36 +15,29 @@ window.onload = () =>{
 
 //------------------------------------------------------excercise button call touchstart function
 
-	document.getElementById("excercise1").addEventListener("touchstart", () =>{buttonPressed(1)});
-	document.getElementById("excercise2").addEventListener("touchstart", () =>{buttonPressed(2)});
-	document.getElementById("excercise3").addEventListener("touchstart", () =>{buttonPressed(3)});
-	document.getElementById("excercise4").addEventListener("touchstart", () =>{buttonPressed(4)});
-	document.getElementById("excercise5").addEventListener("touchstart", () =>{buttonPressed(5)});
-	document.getElementById("excercise6").addEventListener("touchstart", () =>{buttonPressed(6)});
-	document.getElementById("excercise7").addEventListener("touchstart", () =>{buttonPressed(7)});
+	for(i=1; i<=7; i++){
+		let x = (i) =>{
+			document.getElementById(`excercise${i}`).addEventListener("touchstart", () =>{buttonPressed(i)});
+		}
+		x(i);
+	}
+
 //--------//
 
 //------------------------------------------------------excercise button call touchend function
 
-	document.getElementById("excercise1").addEventListener("touchend", () =>{buttonUnpressed(1), excercise1Condition=excerciseCondition(excercise1Condition, 1)});
-	document.getElementById("excercise2").addEventListener("touchend", () =>{buttonUnpressed(2), excercise2Condition=excerciseCondition(excercise2Condition, 2)});
-	document.getElementById("excercise3").addEventListener("touchend", () =>{buttonUnpressed(3), excercise3Condition=excerciseCondition(excercise3Condition, 3)});
-	document.getElementById("excercise4").addEventListener("touchend", () =>{buttonUnpressed(4), excercise4Condition=excerciseCondition(excercise4Condition, 4)});
-	document.getElementById("excercise5").addEventListener("touchend", () =>{buttonUnpressed(5), excercise5Condition=excerciseCondition(excercise5Condition, 5)});
-	document.getElementById("excercise6").addEventListener("touchend", () =>{buttonUnpressed(6), excercise6Condition=excerciseCondition(excercise6Condition, 6)});
-	document.getElementById("excercise7").addEventListener("touchend", () =>{buttonUnpressed(7), excercise7Condition=excerciseCondition(excercise7Condition, 7)});
+	for(i=1; i<=7; i++){
+		let y = (i) =>{
+			document.getElementById(`excercise${i}`).addEventListener("touchend", () =>{buttonUnpressed(i), excerciseCondition[i]=excerciseConditionFunc(excerciseCondition[i], i)});
+		}
+		y(i);
+	}
+
+
 //--------//
 };
 
-let excercise1Condition = true;
-let excercise2Condition = true;
-let excercise3Condition = true;
-let excercise4Condition = true;
-let excercise5Condition = true;
-let excercise6Condition = true;
-let excercise7Condition = true;
-
-function excerciseCondition(excerciseCondition, number){
+function excerciseConditionFunc(excerciseCondition, number){
 	console.log(excerciseCondition);
 	if (excerciseCondition == true){
 		document.getElementById(`listNumbers${number}`).style.cssText = "height:282px;transition:1s;";
@@ -56,7 +49,7 @@ function excerciseCondition(excerciseCondition, number){
 
 }
 //------------------------------------------------button call touchstart function
-function buttonPressed(buttonId){ 
+function buttonPressed(buttonId){
 	document.getElementById(`excercise${buttonId}`).style.cssText = "background:#fff; border-color:#000;color:#000;";
 
 };
@@ -64,6 +57,7 @@ function buttonPressed(buttonId){
 
 //-----------------------------------------------button call touchend function
 function buttonUnpressed(buttonId){
+	console.log(buttonId);
 	document.getElementById(`excercise${buttonId}`).style.cssText = "background:#000; border-color:#fff;color:#fff;";
 };
 //--------//
